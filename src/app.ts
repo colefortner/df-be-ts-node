7256a0338f55a2285faa844d20a8f52db42a7518
Cole Fortner <colefortner@gmail.com>
2022-02-15 14:39:12 -0500

    Import our routes files to app.js and connect them to the express application with app.use()

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from "express";
 
+import businessRoutes from "./routes/businesses";
+import userRoutes from "./routes/users";
+
 const app = express();
+
+app.use("/businesses", businessRoutes);
+app.use("/user", userRoutes);
 
 app.listen(4000);
