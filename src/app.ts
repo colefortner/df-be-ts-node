e89e7b6f522289e8bdd418bd9e319d01f13d3d9b
Cole Fortner <colefortner@gmail.com>
2022-02-15 15:25:41 -0500

    Add a basic catch all error handling middleware, and import the associated types used in express request functions from express

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 import businessRoutes from "./routes/businesses";
 import userRoutes from "./routes/users";
@@ -7,5 +7,9 @@ const app = express();
 
 app.use("/businesses", businessRoutes);
 app.use("/user", userRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(4000);
