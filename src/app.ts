71bfd10a4dca8dacd3e35427b83185c23e063e4e
Cole Fortner <colefortner@gmail.com>
2022-02-17 22:55:59 -0500

    Add CORS middleware to allow front-end requests from our front-end

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,21 @@ import mongoose from "mongoose";
 
 import businessRoutes from "./routes/businesses-routes";
 import userRoutes from "./routes/users-routes";
+import { runInNewContext } from "vm";
 
 const app = express();
 
 app.use(json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
 app.use("/businesses", businessRoutes);
 app.use("/user", userRoutes);
